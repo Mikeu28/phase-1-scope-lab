e52402d5ddde99f9e2f051beadcadd972a7d6a92
Mike Urezzio <mikeurezzio@gmail.com>
2023-03-25 16:04:43 -0400

    Scope lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+var customerName = `bob`;
+const leastFavoriteCustomer = `mike`;
+function upperCaseCustomerName () {
+    customerName = `BOB`;
+} 
+
+function setBestCustomer () {
+    bestCustomer = `not bob`;
+    return bestCustomer;
+}
+
+var overwriteBestCustomer = () => bestCustomer = `maybe bob`;
+
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = `something not mike`;
+}
